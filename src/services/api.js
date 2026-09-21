6d49fe689ebf0ef07ea883c2a64029b4d27fb137
bettyhubcod <ndiayebetty333@gmail.com>
2026-09-21 22:56:15 +0000

    fix: add /api to baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://bantoggback-production.up.railway.app'
+    baseURL: 'https://bantoggback-production.up.railway.app/api'
 });
 
 export const suggererRepas = async (proteines, typeRepas, deviceId) => {
