e534b45bd296519dbcb89befdb0f5baca0132221
bettyhubcod <ndiayebetty333@gmail.com>
2026-09-24 13:30:08 +0000

    ajout de l'Icon

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       manifest: {
-        name: 'BanTogg',
-        short_name: 'BanTogg',
-        description: 'On mange quoi aujourd\'hui ?',
-        theme_color: '#c2703d',
+        name: "BanTogg",
+        short_name: "BanTogg",
+        theme_color: "#c2703d",
         icons: [
-          // tu ajouteras tes icônes ici plus tard (192x192, 512x512)
-        ]
-      }
-    })
-  ]
-})
+          { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
+          { src: "/icon-512.png", sizes: "512x512", type: "image/png" },
+        ],
+      },
+    }),
+  ],
+});
